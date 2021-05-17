29ba7e5da7e5ff88c3054fe5c0ec3e350225b708
nataliao2613 <natalka.o2613@gmail.com>
2021-05-17 23:58:20 +0200

    events added to close and rule buttons

---
+++ src/main.js
@@ -0,0 +1,28 @@
+let rulesButton = document.querySelector('.rules-btn')
+let rules = document.querySelector('.rules')
+let closeButton = document.querySelector('.close-button')
+let show = false;
+
+const onRule = () => {
+    show = true
+    rulesDisplay()
+}
+
+const onClose = () => {
+    show = false
+    rulesDisplay()
+}
+
+const rulesDisplay = () => {
+    if(show){
+        rules.style.display = 'block'
+    }
+    else {
+        rules.style.display = 'none'
+    }
+}
+ 
+
+
+rulesButton.addEventListener('click', onRule)
+closeButton.addEventListener('click', onClose)
