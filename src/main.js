0b4716bdc72732cd2d6d81f1e9f6bff7af9f852a
nataliao2613 <natalka.o2613@gmail.com>
2021-05-18 23:41:42 +0200

    event added to option -choosing one game starts

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 let rulesButton = document.querySelector('.rules-btn')
 let rules = document.querySelector('.rules')
 let closeButton = document.querySelector('.close-button')
-let body = document.querySelector('body')
 let show = false;
+let optionsList = document.querySelectorAll('.options__icon')
+let options = [...optionsList]
+let optionsSection = document.querySelector('.options')
+let gameSection = document.querySelector('.game')
 
 const onRule = () => {
     show = true
@@ -14,16 +17,13 @@ const onClose = () => {
     rulesDisplay()
 }
 
-const rulesDisplay = () => {
-    if(show){
-        rules.style.display = 'block'
-        
-    }
-    else {
-        rules.style.display = 'none'
-    }
+const choseOption = () => {
+    optionsSection.style.display = 'none'
+    gameSection.style.display = 'flex'
 }
- 
+
+const rulesDisplay = () => show ? rules.style.display = 'block' : rules.style.display = 'none'
+options.forEach((op, id) => op.addEventListener('click', choseOption))
 
 
 rulesButton.addEventListener('click', onRule)
