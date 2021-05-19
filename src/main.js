080822ea65620ebd0983ec3f894369a9a20f1646
nataliao2613 <natalka.o2613@gmail.com>
2021-05-19 18:06:36 +0200

    computer choice display

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ let optionsList = document.querySelectorAll('.options__icon')
 let optionsSection = document.querySelector('.options')
 let gameSection = document.querySelector('.game')
 let logo = document.querySelector('header img')
+let userChoice = document.querySelector('.user-choice div')
+let computerChoice = document.querySelector('.computer-choice div')
 
 let show = false
 let options = [...optionsList]
 let moves = ['paper', 'scissors', 'rock']
+let userMove = ''
+let computerMove = ''
 
 const onRule = () => {
     show = true
@@ -25,14 +29,27 @@ const rulesDisplay = () => show ? rules.style.display = 'block' : rules.style.di
 const onLogoClick = () => {
     optionsSection.style.display = 'block'
     gameSection.style.display = 'none'
+    userChoice.classList.remove(`options__icon--${userMove}`)
+}
+
+const computerChooses = () => {
+    let id = Math.floor(Math.random() * moves.length)
+    computerMove = moves[id]
+    computerChoice.classList.add(`options__icon--${computerMove}`)
+    document.querySelector('.computer-choice').classList.add(`computer-choice--${computerMove}`)
+    setTimeout(showComputerChoice, 3000)
+}
+
+const showComputerChoice = () => {
+    document.querySelector('.computer-choice').style.display = 'block'
 }
 
 options.forEach((op, id) => op.addEventListener('click', () => {
     optionsSection.style.display = 'none'
     gameSection.style.display = 'flex'
-    let userChoice = document.querySelector('.user-choice div')
-    userChoice.classList.add(`options__icon--${moves[id]}`)
-
+    userMove = moves[id]
+    userChoice.classList.add(`options__icon--${userMove}`)
+    computerChooses()
 }))
 
 rulesButton.addEventListener('click', onRule)
